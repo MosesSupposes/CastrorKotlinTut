898766b995a9f2b470fcaebb170c86b74c85f007
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-30 16:58:01 -0700

    practice with structural and referential equality

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,19 @@
 fun main() {
-    var message = """
-        Hello, 
-        My name is Moses Samuel.
-        How are you?
-    """.trimIndent()
-    println(message)
+    // structural equality
+    var name1 = "Moses"
+    var name2 = "Moses"
+
+    println(name1 == name2) // true
+    println(listOf("Moses") == listOf("Moses")) // true
+
+    // referential equality
+    var a = Person("Moses")
+    var b = Person("Moses")
+    var c = a
+
+    println(a !== b) // true
+    println(a === c) // true
 }
+
+// classes are hoisted...
+class Person(var name: String)
