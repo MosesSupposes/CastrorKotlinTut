57c8dba50da8e03f323af97a63407aeebe42ae69
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-30 16:22:48 -0700

    practice with strings and chars

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,4 @@
 fun main() {
-    val myByte: Byte = 8 // 8-bit signed interger
-    val myShort: Short = 16 // 16-bit signed integer
-    val myInt: Int = 32 // 32-bit signed integer
-    val myLong: Long = 64 // 64-bit signed integer
-
-    // decimals
-
-    val myFloat: Float = 32.00F // 32-bit floating point number
-    val myDouble: Double = 64.00 // 64-bit floating point number
-
-    val bigLong: Long = 1_000_000 // you can use "_" to delimit large nums
-    println(bigLong)
+    val c: Char = 'a' // chars are delimited by single quotes
+    val firstName = "Moses" // strings are delimited by double quotes
 }
