d5d33cdf4ed2aabc1f60486af694a9f357e1ac2b
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-30 16:19:33 -0700

    practice with numbers

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    println("Hello World")
+    val myByte: Byte = 8 // 8-bit signed interger
+    val myShort: Short = 16 // 16-bit signed integer
+    val myInt: Int = 32 // 32-bit signed integer
+    val myLong: Long = 64 // 64-bit signed integer
+
+    // decimals
+
+    val myFloat: Float = 32.00F // 32-bit floating point number
+    val myDouble: Double = 64.00 // 64-bit floating point number
+
+    val bigLong: Long = 1_000_000 // you can use "_" to delimit large nums
+    println(bigLong)
 }
