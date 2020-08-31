52e42e64e73a874cd6f40b0a461a19e6bbee8049
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-31 16:45:05 -0700

    practice with optional chaining and the elvis operator

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,5 @@
 fun main() {
-    // structural equality
-    var name1 = "Moses"
-    var name2 = "Moses"
-
-    println(name1 == name2) // true
-    println(listOf("Moses") == listOf("Moses")) // true
-
-    // referential equality
-    var a = Person("Moses")
-    var b = Person("Moses")
-    var c = a
-
-    println(a !== b) // true
-    println(a === c) // true
+   val lastName: String? = null
+   println(lastName?.length ?: 0)
 }
 
-// classes are hoisted...
-class Person(var name: String)
