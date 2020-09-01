7ba2fed3a06848dd012079e3c276626037ec559d
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-01 12:12:44 -0700

    practice with default arguments

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,8 @@
 fun main() {
-    printUserInfo("Moses" , "Samuel", age = 22, likesMovies= false, lovesPopcorn = false, isSunburned = false)
+    printUserInfo(age = 22, likesMovies= false, lovesPopcorn = false, isSunburned = false)
 }
 
-fun printUserInfo(firstName: String, lastName: String, age: Int, isSunburned: Boolean, likesMovies: Boolean, lovesPopcorn: Boolean) {
+fun printUserInfo(firstName: String = "John", lastName: String = "Doe", age: Int, isSunburned: Boolean, likesMovies: Boolean, lovesPopcorn: Boolean) {
     println("$firstName $lastName is of age $age. Sunburned: $isSunburned, Likes Movies: $likesMovies, lovesPopcorn: $lovesPopcorn")
 }
 
