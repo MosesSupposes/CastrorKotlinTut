d594f4922f0cf2b42ee0586b568931ddec76abb1
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-01 12:06:59 -0700

    practice with named parameters

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,11 @@
 fun main() {
-   val lastName: String? = null
-   println(lastName?.length ?: 0)
+    printUserInfo(lastName = "Samuel" , firstName = "Moses", age = 22, likesMovies= false, lovesPopcorn = false, isSunburned = false)
 }
+
+fun printUserInfo(firstName: String, lastName: String, age: Int, isSunburned: Boolean, likesMovies: Boolean, lovesPopcorn: Boolean) {
+    println("$firstName $lastName is of age $age. Sunburned: $isSunburned, Likes Movies: $likesMovies, lovesPopcorn: $lovesPopcorn")
+}
+
+
+
 
