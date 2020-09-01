d28eba586d0db2b689b64265733768796f7b73ed
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-01 16:34:25 -0700

    practice with class properties

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,5 @@
 fun main() {
-//    var user = User(firstName = "Moses", lastName = "Samuel" )
-
-//    var friend = User(firstName = "Renee", lastName = "Martinez")
-
-    var cousin = User("Sincere")
+    var user = User(firstName = "Moses", lastName = "Samuel" )
+    println(user.fullName)
 }
 
