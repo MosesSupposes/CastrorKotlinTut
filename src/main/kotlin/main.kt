7199b9fc3612aed9b4f5e05c3ccb63e10617fc62
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-02 14:59:52 -0700

    practice with nested classes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,24 @@
 fun main() {
-    val user = User("Moses", "Samuel")
-    user.favoriteCity = "Minneapolis"
-    println(user.toString())
+    val sw = Vehicle.SteeringWheel()
+    sw.info()
+
+    val transmission = Vehicle.Transmission()
+    transmission.shift()
 }
 
+
+class Vehicle {
+    var brand: String = "unknown"
+
+    fun info() = println(brand)
+
+    class SteeringWheel {
+        val name = "The Steering Wheel"
+        fun info() = println(name)
+    }
+
+    class Transmission {
+        var type = "Automatic"
+        fun shift() = println("The vehicle has shifted")
+    }
+}
