9c0d7b8f5bce7272162f8b4ec36ce5d45f565d9f
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-02 14:47:33 -0700

    practice with constants

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,33 +1,5 @@
 fun main() {
-    println(FavoriteFood.name)
-    FavoriteFood.name = "Watermelon"
-    println(FavoriteFood.name)
-    FavoriteFood.ingredients.add("Salt")
-    println(FavoriteFood.ingredients.first())
-
-    doStuff()
-
-    println(FavoriteFood.name)
-    println(FavoriteFood.ingredients.firstOrNull())
-
-    println(FavoriteFood.numberOfIngredients())
-
-    println(FavoriteFood == FavoriteFood) // true
-    println(FavoriteFood === FavoriteFood) // true
-}
-
-fun doStuff() {
-    FavoriteFood.name = "Chicken"
-    FavoriteFood.ingredients.clear()
-}
-
-// Singleton -> 1 instance
-object FavoriteFood {
-    var name = "unknown"
-    var ingredients = mutableListOf<String>()
-
-    fun numberOfIngredients(): Int {
-        return ingredients.size
-    }
+    Constants.MAX_AGE
+    Constants.MIN_AGE
 }
 
