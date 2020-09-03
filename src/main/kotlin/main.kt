53250e162571de6e3e2f8eea48458dcd64e10321
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-02 18:51:14 -0700

    practice iterating over enums

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,12 @@
 fun main() {
-    val fromApi = "GoLd"
-    val accountType = AccountType.valueOf(fromApi.toUpperCase())
-    println(accountType.calculateDiscountPercent())
+    AccountType.values().forEach { println(it) }
 }
 
 
 
-enum class AccountType(val discountPercent: Int) {
-    BRONZE (5){
-        override fun calculateDiscountPercent(): Int = discountPercent + 10
-    },
-    SILVER(10) {
-        override fun calculateDiscountPercent(): Int  = discountPercent + 10
-    },
-    GOLD(15) {
-        override fun calculateDiscountPercent(): Int = discountPercent + 10
-    },
-    PLATINUM(20) {
-        override fun calculateDiscountPercent(): Int = discountPercent + 10
-    };
-
-    abstract fun calculateDiscountPercent(): Int
+enum class AccountType() {
+    BRONZE,
+    SILVER,
+    GOLD,
+    PLATINUM;
 }
