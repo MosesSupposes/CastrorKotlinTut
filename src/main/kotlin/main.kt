59d962654493721b64a6c0785450487b778abdab
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-03 11:31:12 -0700

    practice with exhaustive and non-exhaustive when statements

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,18 @@
 fun main() {
-   val accounType = (AccountType.getAccountTypeByName("gold"))
-    println(accounType)
+    val accountType = AccountType.GOLD
+
+    // When dealing with a "when" statement, you don't need to exhaustively handle all the cases.
+    when (accountType) {
+        AccountType.PLATINUM -> println("Access allowed")
+    }
+
+    // When dealing with a "when" expression, you must exhaustively handle all the cases.
+    val message = when (accountType) {
+        AccountType.PLATINUM-> "Access allowed"
+        else -> "Not eligible for special access"
+    }
+
+    println(message)
 }
 
 
@@ -10,8 +22,4 @@ enum class AccountType() {
     SILVER,
     GOLD,
     PLATINUM;
-
-    companion object {
-        fun getAccountTypeByName(name: String) = valueOf(name.toUpperCase())
-    }
 }
