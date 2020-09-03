693648dce00fc8543561609c1e5c2911d5f5fb30
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-03 15:16:37 -0700

    practice with protected modifier for methods

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,6 @@
 fun main() {
     val p = Person("Moses", 22)
-    val c = Chef("Bobj", 33, "Ribs")
+    val c = Chef("Bob", 33, "Ribs")
 
-    p.printMyFavoriteFood()
-
-    c.setMyFavoriteFood("Celery")
-    c.printMyFavoriteFood()
+    c.setMyFavoriteFood("Potato")
 }
