301afaca806016b80e2115133d8834e66f9c3657
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-02 18:31:35 -0700

    practice with enums

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,32 +1,17 @@
 fun main() {
-    val myCar = Vehicle()
-    myCar.brand = "Fiat"
-    myCar.info()
+    val accountTypeFromApi = "gold"
+    val accountType = AccountType.valueOf(accountTypeFromApi.toUpperCase())
 
-    // As you can see, we must access the inner class 'Steering Wheel' as an instance of the vehicle class
-    // as opposed to calling it as a static member of the class as we do with 'Transmission' (line 12).
-//    val sw = Vehicle.SteeringWheel()
-    val sw = myCar.SteeringWheel()
-    sw.info()
+    val user = User("Moses", "Samuel", AccountType.PLATINUM)
 
-    val transmission = Vehicle.Transmission()
-    transmission.shift()
+    user.accountType
 }
 
 
-class Vehicle {
-    var brand: String = "unknown"
 
-    fun info() = println(brand)
-
-    // we must declare this subclass as being an 'inner' class in order to access the variables defined outside this class
-    inner class SteeringWheel {
-        val name = "Leather"
-        fun info() = println("$brand is the brand of the vehicle with a $name steering wheel")
-    }
-
-    class Transmission {
-        var type = "Automatic"
-        fun shift() = println("The vehicle has shifted")
-    }
+enum class AccountType {
+    BRONZE,
+    SILVER,
+    GOLD,
+    PLATINUM
 }
