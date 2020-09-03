fd4994cde0c380bd9416c8a9a3caa032ab599f71
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-02 18:47:58 -0700

    practice with abstract functions inside of an enum

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,24 @@
 fun main() {
-   println(AccountType.GOLD)
-    println(AccountType.GOLD.discountPercent)
-    println(AccountType.GOLD.numberOfSubscriptions)
-
-    val value = AccountType.valueOf("Platinum".toUpperCase())
-    println(value)
-    println(value.discountPercent)
-    println(value.numberOfSubscriptions)
+    val fromApi = "GoLd"
+    val accountType = AccountType.valueOf(fromApi.toUpperCase())
+    println(accountType.calculateDiscountPercent())
 }
 
 
 
-enum class AccountType(val discountPercent: Int, val numberOfSubscriptions: Int) {
-    BRONZE(10, 1),
-    SILVER(15, 5),
-    GOLD(20, 10),
-    PLATINUM(25, 15)
+enum class AccountType(val discountPercent: Int) {
+    BRONZE (5){
+        override fun calculateDiscountPercent(): Int = discountPercent + 10
+    },
+    SILVER(10) {
+        override fun calculateDiscountPercent(): Int  = discountPercent + 10
+    },
+    GOLD(15) {
+        override fun calculateDiscountPercent(): Int = discountPercent + 10
+    },
+    PLATINUM(20) {
+        override fun calculateDiscountPercent(): Int = discountPercent + 10
+    };
+
+    abstract fun calculateDiscountPercent(): Int
 }
