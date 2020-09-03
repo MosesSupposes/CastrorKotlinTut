07bf8ede65f3a9f6de3d138ab9787b230aa301bb
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-03 14:45:44 -0700

    practice copying data classes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,21 @@
 fun main() {
-    val p1 = Person("Moses", "Samuel", 22)
+    val person = Person("Moses", "Samuel", 22)
+    val sibling = person.copy(firstName = "Joy", age = 7)
+    println(person)
+    println(sibling)
 
-    // The destructured values below are implemented by calling the .componentN methods on the p1 instance
-    val (firstName, lastName, age) = p1
-    println(firstName)
-    println(lastName)
-    println(age)
+    val order = Order(amount = 100, customer = sibling)
+    println(order)
+
+    val newOrder = order.copy(amount = 200)
+    println(newOrder)
+
+    val newOrder2 = newOrder.copy(customer = person)
+    println(newOrder2)
 }
+
+
+data class Order(val amount: Int, val customer: Person)
 
 // Equals / Hashcode / toString are provided for us
 data class Person(val firstName: String = "John", val lastName: String = "Doe", val age: Int)
