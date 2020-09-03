a3d86a1e99125ff89c09bd69fd1e39362822bc75
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-03 14:50:40 -0700

    practice with pair and triple data classes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,22 +1,11 @@
 fun main() {
-    val person = Person("Moses", "Samuel", 22)
-    val sibling = person.copy(firstName = "Joy", age = 7)
-    println(person)
-    println(sibling)
+//    val pair = Pair("foo", "bar") // this data class comes with Kotlin's standard lib
+    val pair = "foo" to "bar" // this line is equivalent to the line above
+    println(pair.first)
+    println(pair.second)
 
-    val order = Order(amount = 100, customer = sibling)
-    println(order)
-
-    val newOrder = order.copy(amount = 200)
-    println(newOrder)
-
-    val newOrder2 = newOrder.copy(customer = person)
-    println(newOrder2)
+    val triple = Triple("foo", "bar", "baz")
+    println(triple.first)
+    println(triple.second)
+    println(triple.third)
 }
-
-
-data class Order(val amount: Int, val customer: Person)
-
-// Equals / Hashcode / toString are provided for us
-data class Person(val firstName: String = "John", val lastName: String = "Doe", val age: Int)
-
