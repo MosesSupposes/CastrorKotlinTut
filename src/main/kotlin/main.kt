f04ca015671953126d2e8dc278da2a25432ae318
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-06 13:49:48 -0700

    practice with arrays

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,31 +1,18 @@
 fun main() {
-    var button = Button()
-    button.setMyListener(object: OnClickListener {
-        override fun onClick() {
-            println("This was clicked")
-        }
-    })
-    button.click()
+    val items = arrayOf(1,2,3,4,5)
+    items.forEach { println(it) }
+
+    val users = arrayOf(
+        User("Moses", "Samuel"),
+        User("Jane", "Doe")
+    )
+
+    users.forEach { println(it) }
+
+    val updatedUsers = users.plus(User("John", "Doe"))
+    updatedUsers.forEach { println(it) }
+
+    users.set(0, User("Foo", "Bar")) // Arrays are homogenous when dealing with non-primitives (objects)
+    val item = users[0]
+    println(item)
 }
-
-interface OnClickListener {
-    fun onClick()
-}
-
-open abstract class View {
-    lateinit var clickListener: OnClickListener
-
-    fun setMyListener(clickListener: OnClickListener) {
-        this.clickListener = clickListener
-
-    }
-
-    fun click() {
-        clickListener.onClick()
-    }
-}
-
-class Button: View()
-class Image: View()
-class Map: View()
-
