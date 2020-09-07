3716201cd21dbcfbfaa6536864f2278116c7b5cc
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-06 17:41:53 -0700

    practice combining immutable lists

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,10 @@
 fun main() {
-    val names = listOf("Donn", "Bob", "Jane", "Jenny", "Tushar", "Kavita")
-    val filtered = names.filter { it.toLowerCase().contains("a")  }
-    println(filtered)
+    val mine = listOf("Apples", "Grapes")
+    val theirs = listOf("Oranges", "Pears", "Strawberries")
+    val others = listOf("Kiwi", "Watermelon")
 
-    val ages = listOf(23, 33, 12, 9 ,17, 19, 99)
-//    val adults = ages.filter { isAdult(it) }
-    val adults = ages.filter(::isAdult) // this line is equivalent to the line above
-    println(adults)
-}
-fun isAdult(age: Int): Boolean {
-    return age >= 18
+    val result = mine.plus(theirs).plus(others)
+    println(result)
+
+    println(result.minus(theirs))
 }
