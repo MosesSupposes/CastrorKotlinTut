480514f94135cc91f056a20a1cfbeb365b92de63
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-18 15:32:56 -0700

    more practice with for loops

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,5 +3,29 @@ fun main() {
     for (number in values) {
        println(number)
     }
+    println("-------------------------------")
+
+    for (i in 0 until 10) {
+        println(i)
+    }
+    println("-------------------------------")
+
+    for (i in 0 until 100 step 10) {
+        println(i)
+    }
+    println("-------------------------------")
+
+    for (i in 100 downTo 0 step 10) {
+        println(i)
+    }
+    println("-------------------------------")
+
+    val people = listOf(Human("Donn"), Human("Jane"), Human("Karthik"))
+    for (person in people) {
+        println(person.name)
+    }
+
 }
+
+data class Human(val name: String)
 
