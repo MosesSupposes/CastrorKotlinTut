eb3d6c350f544b2123f134e9c5b1114690b9298b
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-18 15:45:13 -0700

    practice with forEach

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,22 @@
 fun main() {
-    var x = 0
-    while (x < 10) {
-        println(x)
-        x++
+    val people = listOf(
+        Human("Donn"),
+        Human("Jake"),
+        Human("Janet"),
+        Human("Kavita")
+    )
+
+    for (person in people) {
+        println(person)
     }
+
+    people.forEach { println(it) }
+
+    
 }
+
+data class Human(val name: String)
+
+
 
 
