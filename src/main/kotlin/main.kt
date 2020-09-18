b7a06cb2b6f24de651da822a41fd3a69aaa544db
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-18 15:37:59 -0700

    practice with while loops

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,31 +1,9 @@
 fun main() {
-    val values = listOf(1,2,3,4,5,6,7)
-    for (number in values) {
-       println(number)
+    var x = 0
+    while (x < 10) {
+        println(x)
+        x++
     }
-    println("-------------------------------")
-
-    for (i in 0 until 10) {
-        println(i)
-    }
-    println("-------------------------------")
-
-    for (i in 0 until 100 step 10) {
-        println(i)
-    }
-    println("-------------------------------")
-
-    for (i in 100 downTo 0 step 10) {
-        println(i)
-    }
-    println("-------------------------------")
-
-    val people = listOf(Human("Donn"), Human("Jane"), Human("Karthik"))
-    for (person in people) {
-        println(person.name)
-    }
-
 }
 
-data class Human(val name: String)
 
